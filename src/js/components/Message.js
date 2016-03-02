2fa89b23a73adf87fcb28cacf7f1e9fbee671f45
Eliseu Benedito Codinhoto <zeucxb@gmail.com>
2016-03-02 20:59:13 -0300

    Message.js comments

--- src/js/components/Message.js
+++ src/js/components/Message.js
@@ -1,6 +1,11 @@
+/*
+	O componente Message é o responsável pela exibição da mensagem,
+	ele basicamente recebe um nome (name) e uma mensage (msg) e
+	retorna o elemento que deve ser exibido.
+*/
 const Message = ({
 	name,
-  msg
+	msg
 }) => {
 	return <div className="message">
   	<h4>{name}</h4>
